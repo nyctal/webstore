402b6d8f8034a9dd67e75f90fd3897333850afc0
nyctal <nyctalnohope@gmail.com>
2016-05-05 12:29:44 +0200

    BugFix Allowed Categories

--- src/main/java/com/nyctalcorp/webstore/validator/AllowedCategories.java
+++ src/main/java/com/nyctalcorp/webstore/validator/AllowedCategories.java
@@ -1,9 +1,11 @@
 package com.nyctalcorp.webstore.validator;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Giacinto on 04/05/2016.
  */
-
+@Component
 public enum AllowedCategories {
     cat1,
     cat2,
