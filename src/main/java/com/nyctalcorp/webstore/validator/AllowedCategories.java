14b429fd0db01eab8370f73c06cad65fdeb53679
nyctal <nyctalnohope@gmail.com>
2016-05-05 12:35:41 +0200

    BugFix Allowed Categories

--- src/main/java/com/nyctalcorp/webstore/validator/AllowedCategories.java
+++ src/main/java/com/nyctalcorp/webstore/validator/AllowedCategories.java
@@ -1,11 +1,9 @@
 package com.nyctalcorp.webstore.validator;
 
-import org.springframework.stereotype.Component;
-
 /**
  * Created by Giacinto on 04/05/2016.
  */
-@Component
+
 public enum AllowedCategories {
     cat1,
     cat2,
--- src/main/java/com/nyctalcorp/webstore/validator/CategoryValidator.java
+++ src/main/java/com/nyctalcorp/webstore/validator/CategoryValidator.java
@@ -11,7 +11,6 @@ import java.util.EnumMap;
 
 public class CategoryValidator implements ConstraintValidator<Category, String>{
 
-    @Autowired
     private AllowedCategories allowedCategories;
 
     public void initialize(Category category) {
